949029099f6bdf3d5bdea5a18cb7cf25e712185f
Julia DeFoor <juliagracedefoor@gmail.com>
2021-03-27 01:28:20 -0400

    Nice Parser simplification

--- src/Parsing.hs
+++ src/Parsing.hs
@@ -20,31 +20,20 @@ pexec :: String -> IO ()
 pexec = either print (print <=< BF.exec) . bfParse ""
 
 bfParseFile :: String -> IO (Either ParseError [BF.Symbol])
-bfParseFile fp = bfParse fp <$> readFile fp
+bfParseFile name = bfParse name <$> readFile name
 
 bfParse :: String -> String -> Either ParseError [BF.Symbol]
-bfParse = parse (bfCode eof)
+bfParse = parse (symbolsTill eof)
 
-bfCode :: Parsec String () a -> Parsec String () [BF.Symbol]
-bfCode terminator = do
-  skipMany fluff
-  result <- nextSymbol
-  continuation result
- where
-  nextSymbol   = (Just <$> symbol) <|> (Nothing <$ terminator)
-  continuation = maybe (return []) (\s -> fmap (s :) (bfCode terminator))
-
-symbol :: Parsec String () BF.Symbol
-symbol = (BF.Instructions <$> instructions) <|> (BF.Loop <$> loop)
+symbolsTill :: Parsec String () a -> Parsec String () [BF.Symbol]
+symbolsTill = manyTill symbol
+  where symbol = (BF.Instructions <$> instructions) <|> (BF.Loop <$> loop)
 
 instructions :: Parsec String () String
 instructions = filter (`elem` BF.characters) <$> many1 (noneOf "[]")
 
 loop :: Parsec String () [BF.Symbol]
-loop = between (char '[') (char ']') (bfCode endOfLoop)
+loop = between (char '[') (char ']') (symbolsTill endOfLoop)
   where endOfLoop = lookAhead $ char ']'
-
-fluff :: Parsec String () Char
-fluff = noneOf BF.characters
 
 
