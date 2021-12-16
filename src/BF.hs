5a0ddb08c5502037798490ff743b040e6abeb3cf
Julia DeFoor <juliagracedefoor@gmail.com>
2021-12-16 03:10:17 -0500

    Improve readability

--- src/BF.hs
+++ src/BF.hs
@@ -12,14 +12,13 @@ import           BF.Util                        ( readChar
                                                 )
 import           BF.Zipper                      ( Zipper )
 import qualified BF.Zipper                     as Zipper
+import           Control.Arrow                  ( (>>>) )
 import           Control.Monad.State
 import           Data.Word                      ( Word8 )
 
-type BF a = StateT TapeMachine IO a
-
 type TapeMachine = Zipper Word8
 
-data Instruction = MoveLeft | MoveRight | AddOne | SubtractOne | WriteChar | ReadChar | Loop [Instruction]
+data Instruction = MoveLeft | MoveRight | AddOne | SubtractOne | WriteChar | ReadChar | Loop [Instruction] deriving (Show)
 
 eval :: [Instruction] -> IO ()
 eval = void . exec
@@ -30,15 +29,15 @@ exec = runInstructionsOn $ Zipper.new 30000
 runInstructionsOn :: TapeMachine -> [Instruction] -> IO TapeMachine
 runInstructionsOn tm xs = execStateT (mapM_ perform xs) tm
 
-perform :: Instruction -> BF ()
+perform :: Instruction -> StateT TapeMachine IO ()
 perform MoveLeft    = modify Zipper.left
 perform MoveRight   = modify Zipper.right
 perform AddOne      = modify $ Zipper.alter (+ 1)
 perform SubtractOne = modify $ Zipper.alter (subtract 1)
-perform WriteChar   = gets Zipper.focus >>= liftIO . writeChar
-perform ReadChar    = liftIO readChar >>= modify . Zipper.alter . maybe id const
-perform l@(Loop xs) = do
-  x <- gets Zipper.focus
-  unless (x == 0) $ do
+perform WriteChar   = liftIO . writeChar =<< gets Zipper.focus
+perform ReadChar    = modify . Zipper.alter . maybe id const =<< liftIO readChar
+perform (Loop xs)   = loop =<< gets Zipper.focus
+ where
+  loop x = unless (x == 0) $ do
     mapM_ perform xs
-    perform l
+    perform (Loop xs)
--- src/BF/Stateful.hs
+++ src/BF/Stateful.hs
@@ -1,14 +1,23 @@
-module BF.Stateful where
+module BF.Stateful
+  ( eval
+  )
+where
 
 import           BF                             ( Instruction(..) )
-import           BF.Util                        ( readChar
-                                                , writeChar
+import           BF.Util                        ( writeChar
+                                                , readChar
                                                 )
-import           Control.Monad                  ( unless )
-import           Control.Monad.ST
+import           Control.Monad                  ( unless
+                                                , (<=<)
+                                                )
+import           Control.Monad.ST               ( RealWorld )
+import           Data.IORef                     ( modifyIORef'
+                                                , newIORef
+                                                , readIORef
+                                                , IORef
+                                                )
 import           Data.Vector.Unboxed.Mutable    ( MVector )
 import qualified Data.Vector.Unboxed.Mutable   as MV
-import           Data.IORef
 import           Data.Word                      ( Word8 )
 
 size :: Int
@@ -18,25 +27,35 @@ eval :: [Instruction] -> IO ()
 eval instructions = do
   cells   <- MV.replicate size (0 :: Word8)
   pointer <- newIORef (0 :: Int)
-  mapM_ (performIO cells pointer) instructions
+  mapM_ (perform cells pointer) instructions
 
-performIO :: MVector RealWorld Word8 -> IORef Int -> Instruction -> IO ()
-performIO _ pointer MoveLeft  = modifyIORef' pointer ((`mod` size) . subtract 1)
-performIO _ pointer MoveRight = modifyIORef' pointer ((`mod` size) . (+) 1)
-performIO cells pointer AddOne =
-  readIORef pointer >>= MV.unsafeModify cells (+ 1)
-performIO cells pointer SubtractOne =
-  readIORef pointer >>= MV.unsafeModify cells (subtract 1)
-performIO cells pointer WriteChar =
-  readIORef pointer >>= MV.unsafeRead cells >>= writeChar
-performIO cells pointer ReadChar = do
-  i <- readIORef pointer
-  c <- readChar
-  let f = maybe id const c
-  MV.unsafeModify cells f i
-performIO cells pointer l@(Loop xs) = do
-  i <- readIORef pointer
-  x <- MV.unsafeRead cells i
-  unless (x == 0) $ do
-    mapM_ (performIO cells pointer) xs
-    performIO cells pointer l
+perform :: MVector RealWorld Word8 -> IORef Int -> Instruction -> IO ()
+-- Move Left (<)
+perform _ pointer MoveLeft  = modifyIORef' pointer ((`mod` size) . subtract 1)
+
+-- Move Right (>)
+perform _ pointer MoveRight = modifyIORef' pointer ((`mod` size) . (+ 1))
+
+-- Add One (+)
+perform cells pointer AddOne =
+  MV.unsafeModify cells (+ 1) <=< readIORef $ pointer
+
+-- Subtract One (-)
+perform cells pointer SubtractOne =
+  MV.unsafeModify cells (subtract 1) <=< readIORef $ pointer
+
+-- Write Character (.)
+perform cells pointer WriteChar =
+  writeChar <=< MV.unsafeRead cells <=< readIORef $ pointer
+
+-- Read Character (,)
+perform cells pointer ReadChar = modifyWith . maybe id const =<< readChar
+  where modifyWith f = MV.unsafeModify cells f <=< readIORef $ pointer
+
+-- Loop instructions ([])
+perform cells pointer (Loop xs) =
+  loop <=< MV.unsafeRead cells <=< readIORef $ pointer
+ where
+  loop x = unless (x == 0) $ do
+    mapM_ (perform cells pointer) xs
+    perform cells pointer (Loop xs)
